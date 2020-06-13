7ec3a17ee557a38b761c8f5e7c92d2619e070337
liuyuan <ly6819@hotmail.com>
2020-06-13 16:58:10 +0800

    init

---
+++ routes/home.js
@@ -0,0 +1,24 @@
+const router = require('koa-router')()
+const {logger} = require('../config/config')
+
+router.get('/', async (ctx, next) => {
+        await ctx.render('index', {
+            title: 'Hello Koa 2!'
+        })
+        next()
+    }, function() {
+        logger.debug('Hello Koa 2!')
+    }
+)
+
+router.get('/string', async (ctx, next) => {
+    ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+    ctx.body = {
+        title: 'koa2 json'
+    }
+})
+
+module.exports = router
