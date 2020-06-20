a2c6cc4f74e88f75e598c2ec4531eb59ee5ee21d
liuyuan <ly6819@hotmail.com>
2020-06-21 01:23:45 +0800

    增加依赖

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const {logger: appLogger} = require('./config/config')
+const bodyParser = require('koa-bodyparser')
+const Router = require('koa-router');
+const router = new Router();
+const koaLogger = require('koa-logger')
+const {logger} = require('./config/config')
 
 const home = require('./routes/home')
 const users = require('./routes/users')
@@ -13,12 +15,12 @@ const users = require('./routes/users')
 // error handler
 onerror(app)
 
-// middlewares
-app.use(bodyparser({
+// middleware
+app.use(bodyParser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
+app.use(koaLogger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -30,16 +32,17 @@ app.use(async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
-  appLogger.info(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  logger.info(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 // routes
-app.use(home.routes(), home.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(home.routes())
+app.use(users.routes())
+app.use(router.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  appLogger.error('server error', err, ctx)
+  logger.error('server error', err, ctx)
 });
 
 module.exports = app
